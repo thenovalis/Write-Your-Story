4b87f2c7cf40bc13a5fe92caca16ab20cfc26ecc
thenovalis <136692730+thenovalis@users.noreply.github.com>
2023-07-19 18:11:29 +0700

    Fix memory leaks

---
+++ island_mountain_waterfall.ts
@@ -0,0 +1,48 @@
+// Story.ts
+
+// Write Your Story
+
+// Introduction
+
+let storyName: string = 'The Journey of a Hero';
+let authorName: string = 'John Martin Smith';
+let storySynopsis: string = 'The story of a brave knight on a quest to save his kingdom from a powerful evil';
+
+console.log(`This is the story of ${storyName}, written by ${authorName}. It is about a brave knight on a quest to save his kingdom from a powerful evil.`);
+
+// Chapter 1
+
+let knightName: string = 'Sir Robert';
+let kingdomName: string = 'Orlandia';
+let villainName: string = 'Lord Darkfang';
+let knightStrength: number = 10;
+let knightWeakness: string = 'Fear of failure';
+
+console.log(`${knightName} was a knight of the kingdom of ${kingdomName}. His mission was to defeat ${villainName}, the dark lord who threatened to overtake the kingdom. He marched bravely forward, despite his fear of failure.  He was strong and determined, but his strength was only ${knightStrength}.`);
+
+// Chapter 2
+
+let princessName: string = 'Princess Orla';
+let adviceGiven: string = 'Trust in yourself';
+let princessStrength: number = 8;
+
+console.log(`On his journey, ${knightName} encountered ${princessName}. She told him that he must overcome his fear if he was to fulfill his quest. She advised him to ${adviceGiven} and offered her own strength of ${princessStrength} as an added boost.`);
+
+// Chapter 3
+
+let magicalItemName: string = 'Excalibur';
+let magicalItemStrength: number = 15;
+let magicalItemWeakness: string = 'A broken heart';
+
+console.log(`${knightName} then stumbled upon ${magicalItemName}, a magical sword that was said to be able to grant its wielder strength beyond measure. When he used it, its strength was ${magicalItemStrength}, though it had one weakness - ${magicalItemWeakness}.`);
+
+// Chapter 4
+
+let battleName: string = 'The Battle of Orlandia';
+let battleConclusion: string = 'victory';
+
+console.log(`With the added strength of the magical sword, ${knightName} was able to finally face ${villainName} in the great battle of ${battleName}. In the end, it was ${battleConclusion} for the brave knight and his kingdom was saved.`);
+
+// Epilogue
+
+console.log(`And that was the story of ${storyName}, written by ${authorName}. It was a story of courage and determination in the face of insurmountable odds. It teaches us that strength comes not only from physical strength, but also from having faith in ourselves.`);
