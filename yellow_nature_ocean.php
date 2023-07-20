c2335a7b56ff809582c699a098a6d046e0ff012c
thenovalis <136692730+thenovalis@users.noreply.github.com>
2023-07-20 20:23:58 +0700

    Fix UI layout issues

---
+++ yellow_nature_ocean.php
@@ -0,0 +1,80 @@
+<?php
+
+//Write Your Story :
+
+//Story 1
+$story1 = <<<EOT
+Once upon a time, there was a beautiful princess living in a far away kingdom. She had long, flowing black hair, a bright smile, and a kind heart.
+
+She was beloved by her people and she was happy and content with her life.
+
+One day, however, a wicked witch appeared at her castle and cast a spell of darkness over the entire kingdom.
+
+The princess was distraught and set off on a quest to find a way to break the witch's spell. She ventured into deep forests, crossed dangerous mountain ranges, and swam in raging rivers.
+
+After weeks of searching, she finally stumbled upon an enchanted lake deep in the heart of the kingdom. There, at the center of the lake, she found a magical sword that was said to have the power to break any curse.
+
+The princess took the sword and returned to the castle. She challenged the witch to a battle and, with the magical blade in her hand, she was able to defeat the evil witch and restore peace and joy to the kingdom.
+
+The princess lived happily ever after and her heroic tale is still told to this day.
+EOT;
+
+//Story 2
+$story2 = <<<EOT
+Once upon a time, there was a brave knight living in a small village. He was renowned for his courage and strength and was respected by all who knew him.
+
+One day, however, his village was attacked by an evil dragon. The knight stepped up to defend his home, but the dragon was too powerful.
+
+The knight, not wanting to give up, decided to seek help. He traveled far and wide, looking for someone who could help him defeat the dragon.
+
+Eventually, he came across a wise old wizard, who gave him an enchanted sword. With the magical blade in his hand, the knight was able to battle the dragon and, with a few strikes, the dragon was defeated.
+
+The knight returned to his village in triumph and was hailed as a hero. He was celebrated for his bravery and courage, and his story spread throughout the land.
+
+The knight and his village remained safe for the rest of their days and the knight's heroic tale is still told to this day.
+EOT;
+
+//Story 3
+$story3 = <<<EOT
+Once upon a time, there was a young wizard living in a peaceful kingdom. He was skilled and powerful, and he used his magic to help those in need.
+
+One day, however, his kingdom was threatened by a fierce army of goblins. The wizard knew that he had to do something to help protect his people. 
+
+He cast a powerful spell that summoned a mighty dragon to come to his aid. The dragon arrived, and with its help, the wizard was able to defeat the goblins and save the kingdom.
+
+The citizens of the kingdom celebrated the wizard's bravery and thanked him for his service. From that day onward, the wizard lived a long and peaceful life, remembered for his heroic deeds.
+EOT;
+
+//Story 4
+$story4 = <<<EOT
+Once upon a time, there was a powerful sorceress living in a far away land. She was feared and respected by all who knew her, for she held immense powers and could control the elements with a single thought.
+
+One day, however, her beloved kingdom was threatened by a dark force. The sorceress knew she had to do something to protect her people, so she cast a powerful spell that summoned a massive army of magical creatures.
+
+The creatures fought alongside the sorceress and, with their help, the kingdom was saved from destruction. The people of the kingdom thanked the sorceress for her bravery and heroism, and she was celebrated for her courage and strength.
+
+The sorceress lived happily ever after, and her heroic tale is still told to this day.
+EOT;
+
+//Story 5
+$story5 = <<<EOT
+Once upon a time, there was a brave warrior living in a kingdom far away. He was renowned for his courage and strength, and he had the respect of his people. 
+
+One day, however, his kingdom was attacked by a powerful enemy. The warrior stepped up to defend his home, but the enemy was too strong for him to handle alone.
+
+Desperate for help, the warrior traveled to a distant land in search of an ancient weapon that was said to have the power to defeat any foe. After weeks of searching, he finally stumbled across the weapon and wielded it with great strength.
+
+With the magical weapon at his side, the warrior was able to defeat the enemy and save his kingdom. He was hailed as a hero and his name was celebrated across the land.
+
+The warrior lived happily ever after, and his heroic tale is still told to this day.
+EOT;
+
+echo $story1;
+echo "<br><br>";
+echo $story2;
+echo "<br><br>"; 
+echo $story3;
+echo "<br><br>";
+echo $story4;
+echo "<br><br>";
+echo $story5;
